4d4fe83b6d43b3a334406c96662ad31167853fd7
Christian Muehlhaeuser <muesli@gmail.com>
2022-01-19 15:51:13 +0100

    Repository: don't crash accessing invalid pathinfo (#443)
    When fs.Stat returns an error, pathinfo may be nil. In such situations
    the only safe response seems to be to return the error to the caller.
    
    Without this fix, accessing pathinfo.IsDir() below would lead to a crash
    dereferencing a nil pointer.
    
    This crash can be reproduced by trying to initialize a Git repo with an
    invalid path name.
    
    Also see: https://github.com/muesli/gitty/issues/36

--- repository_test.go
+++ repository_test.go
@@ -3005,6 +3005,11 @@ func (s *RepositorySuite) TestBrokenMultipleShallowFetch(c *C) {
 	c.Assert(err, IsNil)
 }
 
+func (s *RepositorySuite) TestDotGitToOSFilesystemsInvalidPath(c *C) {
+	_, _, err := dotGitToOSFilesystems("\000", false)
+	c.Assert(err, NotNil)
+}
+
 func BenchmarkObjects(b *testing.B) {
 	defer fixtures.Clean()
 
